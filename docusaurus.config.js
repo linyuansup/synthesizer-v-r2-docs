031efb4e431e89bcbc0d1793336803a17bfe8127
linyuansup <linyuansup@gmail.com>
2021-12-01 13:14:12 +0800

    update qingsu

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -5,7 +5,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 /** @type {import('@docusaurus/types').Config} */
 const config = {
   title: 'Synthesizer V Studio 用户手册',
-  tagline: '编辑器信息适配到 Synthesizer V Studio 1.4.0，声库信息更新到爱莲娜 芙缇 AI Lite，更新于 2021 / 11 / 29',
+  tagline: '编辑器信息适配到 Synthesizer V Studio 1.4.0，声库信息更新到青溯，更新于 2021 / 12 / 01',
   url: 'https://synthesizer-v-r2-docs.vercel.app/',
   baseUrl: '/',
   onBrokenLinks: 'throw',
@@ -167,7 +167,7 @@ const config = {
             ],
           },
         ],
-        copyright: `磷元素P & 不羁阁 - Synthesizer V Studio Docs 更新于 2021 / 11 / 29 适配到 Synthesizer V Studio 1.4.0`,
+        copyright: `磷元素P & 不羁阁 - Synthesizer V Studio Docs 更新于 2021 / 12 / 01 适配到 Synthesizer V Studio 1.4.0`,
       },
       prism: {
         theme: lightCodeTheme,
